b873d7e9209822c300f198731870d5f572f3549d
Dhruvik Navadiya <navadiyadhruvik2@gmail.com>
2019-08-15 14:37:31 +0530

    basic structure

--- app/src/main/java/com/example/dhruvik/caloriecalculator/CalorieCounter.java
+++ app/src/main/java/com/example/dhruvik/caloriecalculator/CalorieCounter.java
@@ -3,11 +3,11 @@ package com.example.dhruvik.caloriecalculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Home extends AppCompatActivity {
+public class CalorieCounter extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_calorie_counter);
     }
 }
